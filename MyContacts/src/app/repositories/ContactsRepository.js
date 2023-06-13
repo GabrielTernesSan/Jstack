862a11357f5ba7b9b1c4a29219d233b00db29c0d
GabrielTernesSan <santos.tgabriel@hotmail.com>
2023-06-13 08:54:45 -0300

    Refatorando o método create

--- MyContacts/src/app/repositories/ContactsRepository.js
+++ MyContacts/src/app/repositories/ContactsRepository.js
@@ -20,16 +20,16 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.id === id));
-    });
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
   delete(id) {
@@ -39,10 +39,10 @@ class ContactsRepository {
     });
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.email === email));
-    });
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+
+    return row;
   }
 
   async create({
